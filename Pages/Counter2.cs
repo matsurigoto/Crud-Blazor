7ab9e28e6932f5b4007ab9349f9b9e0193070ee9
Administrator <admin@example.com>
2021-12-15 14:07:21 +0800

    first commit

---
+++ Pages/Counter2.cs
@@ -0,0 +1,12 @@
+namespace Crud_Blazor.Pages{
+
+    public partial class Counter2 {
+        private int currentCount = 0;
+        private string name = "Duran";
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
